b994e9dad548763b0a848b113359216ef38487c4
Vadym Gon <vadim1goncharenko@gmail.com>
2023-03-03 22:41:03 +0100

    Extension for variation of k

--- src/Solution.java
+++ src/Solution.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class Solution {
-    public static String removeAdjacentDuplicates(String data) {
+    public static String removeAdjacentDuplicates(String data, int k) {
 
         Stack<Character> stack = new Stack<>();
 
@@ -10,11 +10,24 @@ public class Solution {
 
         for (int i = 0; i < arr.length; i++) {
 
-            if (!stack.empty() && stack.peek().charValue() == arr[i]) {
-                stack.pop();
+
+            if (!stack.empty() && stack.size() >= k - 1 && stack.peek().charValue() == arr[i]) {
+
+                boolean allSame = true;
+                for (int j = 1; j < k; j++) {
+                    if (!(stack.get(stack.size() - j).charValue() == arr[i])) {
+                        allSame = false;
+                    }
+                }
+                if (!allSame) {
+                    stack.push(arr[i]);
+                } else {
+                    for (int q = 1; q < k; q++) {
+                        stack.pop();
+                    }
+                }
             } else {
                 stack.push(arr[i]);
-
             }
         }
         return stack.stream().map(i -> i.toString()).collect(Collectors.joining(""));
--- src/SolutionTest.java
+++ src/SolutionTest.java
@@ -4,32 +4,47 @@ class SolutionTest {
 
     @org.junit.jupiter.api.Test
     void simpleTest1() {
-        assertEquals("ps", Solution.removeAdjacentDuplicates("pbbcggttciiippooaais"));
-        //pbcgtcipoais
+        assertEquals("ps", Solution.removeAdjacentDuplicates("pbbcggttciiippooaais", 2));
+
     }
 
     @org.junit.jupiter.api.Test
     void simpleTest2() {
-        assertEquals("abcd", Solution.removeAdjacentDuplicates("abcd"));
+        assertEquals("abcd", Solution.removeAdjacentDuplicates("abcd", 2));
     }
 
     @org.junit.jupiter.api.Test
     void simpleTest3() {
-        assertEquals("i", Solution.removeAdjacentDuplicates("iii"));
+        assertEquals("i", Solution.removeAdjacentDuplicates("iii", 2));
     }
 
     @org.junit.jupiter.api.Test
     void simpleTest4() {
-        assertEquals("", Solution.removeAdjacentDuplicates("iibb"));
+        assertEquals("", Solution.removeAdjacentDuplicates("iibb", 2));
     }
 
     @org.junit.jupiter.api.Test
     void simpleTest5() {
-        assertEquals("", Solution.removeAdjacentDuplicates("iibbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"));
+        assertEquals("", Solution.removeAdjacentDuplicates("iibbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", 2));
     }
 
     @org.junit.jupiter.api.Test
     void simpleTest6() {
-        assertEquals("i", Solution.removeAdjacentDuplicates("i"));
+        assertEquals("i", Solution.removeAdjacentDuplicates("i", 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void simpleTest7() {
+        assertEquals("aa", Solution.removeAdjacentDuplicates("deeedbbcccbdaa", 3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void simpleTest8() {
+        assertEquals("r", Solution.removeAdjacentDuplicates("reee", 3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void simpleTest9() {
+        assertEquals("wwwu", Solution.removeAdjacentDuplicates("wwwqqqqqu", 5));
     }
 }
